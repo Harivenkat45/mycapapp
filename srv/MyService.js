7a299ef8b2b00772df80d778c8a66e2e76428c13
Harish V <harivenkat0506@gmail.com>
2026-09-17 04:00:19 +0000

    New change2

--- srv/MyService.js
+++ srv/MyService.js
@@ -7,7 +7,7 @@ module.exports = class MyService extends cds.ApplicationService { init() {
   this.on ('harish', async (req) => {
     console.log('On harish', req.data)
     let name = req.data.name;
-    return `Hi ${name} welcome to world of BTP CAPM.`
+    return `Hi ${name} welcome to world of BTP CAPM`
   })
 
   return super.init()
